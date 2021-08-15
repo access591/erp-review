b68e29c333ea9196c776260e307218981e1b0bd0
rahultiwari07 <accessb31@gmail.com>
2021-08-15 10:51:26 +0530

    code commit@rahul
    fixing layout

--- src/main/java/com/access/erp/controller/loginContoller.java
+++ src/main/java/com/access/erp/controller/loginContoller.java
@@ -15,29 +15,9 @@ public class loginContoller {
 	
 	@GetMapping("/index")
 	public String indexPage(Model model) {
-		return "index";
+		return "layouts/index";
 	}
 	
-	@GetMapping("/navbar")
-	public String navbarPAge(Model model) {
-		return "fragments/navbar";
-	}
-	
-	@GetMapping("/sidebar")
-	public String sidebarPage(Model model) {
-		return "fragments/sidebar";
-	}
-	
-	
-	@GetMapping("/navbar1")
-	public String navbarPAge1(Model model) {
-		return "fragments/navbar1";
-	}
-	
-	@GetMapping("/sidebar1")
-	public String sidebarPage1(Model model) {
-		return "fragments/sidebar1";
-	}
 	
 	
 
