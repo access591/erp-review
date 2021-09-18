ccb5b541f8ee84d15b2ddd7ede86140de490a59d
Access Infotech India Pvt Ltd <66505512+access591@users.noreply.github.com>
2021-09-18 10:12:38 +0530

    code commit@rahul
    design part of Request for Quotation has done

---
+++ src/main/java/com/access/erp/controller/RequestQuotationController.java
@@ -0,0 +1,16 @@
+package com.access.erp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/requestquotation")
+public class RequestQuotationController {
+
+	@GetMapping("/")
+	public String requestQuotation() {
+		
+		return "layouts/Master/requestForQuotation";
+	}
+}
