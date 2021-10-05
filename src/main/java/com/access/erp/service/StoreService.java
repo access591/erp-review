07046e12d0a8e8c0a655c74e1705385d394dda54
Access Infotech India Pvt Ltd <accessb31@gmail.com>
2021-10-05 14:11:30 +0530

    code commit@Rahul
    design and devlopment of category master form and store master form

---
+++ src/main/java/com/access/erp/service/StoreService.java
@@ -0,0 +1,15 @@
+package com.access.erp.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.access.erp.model.master.Store;
+
+public interface StoreService {
+
+	public void addStore(Store store);
+	public List<Store> getAllStore();
+	public Optional<Store> editStore(String storeCode);
+	public void deleteStore(String storeCode);
+	
+}
---
+++ src/main/java/com/access/erp/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,52 @@
+package com.access.erp.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.access.erp.model.master.Category;
+import com.access.erp.repo.CategoryRepo;
+import com.access.erp.repo.SeqMainRepo;
+import com.access.erp.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+	
+	@Autowired CategoryRepo categoryRepo;
+	@Autowired SeqMainRepo seqMainRepo;
+	
+	@Override
+	public void addCategory(Category category) {
+		
+		if (category.getCategCode() == "" || category.getCategCode() == null) {
+			System.out.println("category code is : "+ category.getCategCode());
+			String maxCode = seqMainRepo.findByTranType("CAT");
+			category.setCategCode(maxCode);
+		}
+
+		categoryRepo.save(category);
+		
+	}
+
+	@Override
+	public List<Category> getAllCategory() {
+		
+		return this.categoryRepo.findAll();
+	}
+
+	@Override
+	public Optional<Category> editCategory(String CategoryCode) {
+
+		return this.categoryRepo.findById(CategoryCode);
+	}
+
+	@Override
+	public void deleteCategory(String CategoryCode) {
+		this.categoryRepo.deleteById(CategoryCode);
+		
+	}
+
+}
