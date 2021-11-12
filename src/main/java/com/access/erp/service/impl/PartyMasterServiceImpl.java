d1d9f65ec3f8eacc846746b5c63d43a6a3131f75
Access Infotech India Pvt Ltd <accessb31@gmail.com>
2021-11-12 14:50:12 +0530

    code commit@Rahul
    ui has been done of party master form

--- src/main/java/com/access/erp/service/impl/PartyMasterServiceImpl.java
+++ src/main/java/com/access/erp/service/impl/PartyMasterServiceImpl.java
@@ -1,10 +1,38 @@
 package com.access.erp.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.access.erp.model.master.PartyMaster;
 import com.access.erp.service.PartyMasterService;
 
 @Service
 public class PartyMasterServiceImpl implements PartyMasterService{
+
+	@Override
+	public void addPartyMaster(PartyMaster city) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PartyMaster> getAllpartyMaster() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<PartyMaster> editPartyMaster(String partyCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePartMaster(String partyCode) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
