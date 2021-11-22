434f3bd32cb40285a6411c234056c6e97fc6d3b1
Access Infotech India Pvt Ltd <accessb31@gmail.com>
2021-11-22 12:15:41 +0530

    code commit@Rahul
    import dynamic data from backend on popup module while login  and add dynamic dropdown for active financial against company

--- src/main/java/com/access/erp/controller/loginContoller.java
+++ src/main/java/com/access/erp/controller/loginContoller.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.access.erp.model.master.Company;
+import com.access.erp.model.master.FinancialActiveYear;
 import com.access.erp.service.CompanyService;
 import com.access.erp.service.FinancialActiveYearService;
 
@@ -23,11 +26,11 @@ public class loginContoller {
 	FinancialActiveYearService financialActiveyearService;
 	@Autowired
 	CompanyService companyService;
+	
 
 	@GetMapping("/verify")
 	public String indexPage(Model model) {
 
-		financialActiveyearService.getAllFinancialActiveYear();
 
 		List<Company> listCompany = companyService.getAllCompany();
 		model.addAttribute("listCompany", listCompany);
@@ -39,6 +42,21 @@ public class loginContoller {
 	public String loginPopupPage(Model model) {
 
 		return "layouts/index";
+	}
+	
+	
+	@ResponseBody
+	@GetMapping("/activefy/{id}")
+	public List<FinancialActiveYear> getStateCountryById(@PathVariable(value = "id") String compCode) {
+		
+		Company company = companyService.editCompany(compCode);
+		
+		System.out.println("Company code Is : " + company.getCompCode());
+		List<FinancialActiveYear> fy =  financialActiveyearService.findByCompany(company);
+		System.out.println("fy size : " + fy.size());
+		return fy;
+		
+		//return null;
 	}
 
 }
