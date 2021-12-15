61ce6a3f14537ee05dae9118aac978f338d41b67
Access Infotech India Pvt Ltd <accessb31@gmail.com>
2021-12-15 10:42:10 +0530

    code commit@Rahul
    update city state against country n update city against state

--- src/main/java/com/access/erp/controller/master/CountryController.java
+++ src/main/java/com/access/erp/controller/master/CountryController.java
@@ -14,71 +14,112 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.access.erp.model.master.City;
 import com.access.erp.model.master.Country;
+import com.access.erp.model.master.State;
+import com.access.erp.service.CityService;
 import com.access.erp.service.CountryService;
+import com.access.erp.service.StateService;
 
 @Controller
 @RequestMapping("/country")
 public class CountryController {
-	
-	@Autowired CountryService countryService;
-	
+
+	@Autowired
+	CountryService countryService;
+	@Autowired
+	StateService stateService;
+	@Autowired
+	CityService cityService;
+
 	@GetMapping("/")
 	public String country(Model model) {
-		
 
 		model.addAttribute("country", new Country());
 		return "layouts/Master/country";
 	}
-	
+
 	@PostMapping("/")
-	public String addCountry(@ModelAttribute("country") Country country,Principal principal) {
-		
+	public String addCountry(@ModelAttribute("country") Country country, Principal principal) {
+
 		country.setInsertedBy(principal.getName());
 		country.setInsertedDate(new Date());
-		
+
 		countryService.addCountry(country);
-		
+
 		return "redirect:/country/";
 	}
-	
+
 	@GetMapping("/list")
 	public String viewCountryList(Model model) {
-		
+
 		List<Country> countryList = countryService.getAllCountry();
-		
-		if(countryList != null) {
+
+		if (countryList != null) {
 			model.addAttribute("countryList", countryList);
 		}
 		return "layouts/listview/listofcountry";
 	}
-	
+
 	@GetMapping("/edit/{id}")
-	public String editCountry(@PathVariable("id") String countryCode,Model model) {
-		
-		
-		System.out.println("module code is : "+ countryCode);
+	public String editCountry(@PathVariable("id") String countryCode, Model model) {
+
+		System.out.println("module code is : " + countryCode);
 		Optional<Country> country = countryService.editCountry(countryCode);
 		model.addAttribute("country", country);
 		return "layouts/editview/editCountry";
 	}
-	
+
 	@GetMapping("/delete/{id}")
-	public String deleteCountry(@PathVariable("id") String countryCode,Model model) {
+	public String deleteCountry(@PathVariable("id") String countryCode, Model model) {
 		countryService.deleteCountry(countryCode);
 		return "redirect:/country/list";
 	}
-	
+
 	@PostMapping("/update")
-	public String updateCountry(@ModelAttribute("country") Country country,Principal principal) {
-		
+	public String updateCountry(@ModelAttribute("country") Country country, Principal principal) {
+
 		country.setUpdateBy(principal.getName());
 		country.setUpdatedDate(new Date());
+
+		System.out.println("country status is  : " + country.getActive());
+
+		if (country.getActive().equals("N")) {
+
+			List<State> stateList = stateService.findByCountry(country);
+
+			for (State state : stateList) {
+				state.setActive("N");
+				stateService.addState(state);
+
+				List<City> cityList = cityService.findByState(state);
+
+				for (City city : cityList) {
+					city.setActive("N");
+					cityService.addCity(city);
+				}
+			}
+		} else {
+
+			List<State> stateList = stateService.findByCountry(country);
+
+			for (State state : stateList) {
+				state.setActive("Y");
+				stateService.addState(state);
+
+				List<City> cityList = cityService.findByState(state);
+
+				for (City city : cityList) {
+					city.setActive("Y");
+					cityService.addCity(city);
+				}
+			}
+
+		}
+
 		countryService.addCountry(country);
-		
+
 		return "redirect:/country/";
 	}
-	
 
 }
--- src/main/java/com/access/erp/repo/StateRepo.java
+++ src/main/java/com/access/erp/repo/StateRepo.java
@@ -1,9 +1,14 @@
 package com.access.erp.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.access.erp.model.master.Country;
 import com.access.erp.model.master.State;
 
 public interface StateRepo extends JpaRepository<State, String>{
+	
+	List<State> findByCountry(Country country);
 
 }
