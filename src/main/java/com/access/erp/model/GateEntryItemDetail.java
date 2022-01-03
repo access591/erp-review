20385b72e2b015456b427e0ccc4ae0d0677f80b1
Access Infotech India Pvt Ltd <accessb31@gmail.com>
2022-01-03 14:19:03 +0530

    code commit@Rahul
    modification in gate entry form and edit form

--- src/main/java/com/access/erp/model/GateEntryItemDetail.java
+++ src/main/java/com/access/erp/model/GateEntryItemDetail.java
@@ -11,9 +11,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.access.erp.model.master.Item;
 import com.access.erp.model.master.UOM;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 
 @Entity
@@ -46,6 +49,8 @@ public class GateEntryItemDetail {
 	@ManyToOne
 	private PurchaseOrder purchaseOrder;
 	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	@Column(name="ORDER_DATE")
 	private Date orderDate;
 	
