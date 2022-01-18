09481da7f369a119d48efbdc7cbf8a72576437c3
Access Infotech India Pvt Ltd <accessb31@gmail.com>
2022-01-18 10:06:27 +0530

    code commit@Rahul
    bug resolution

--- src/main/java/com/access/erp/repo/RFQuotationRepo.java
+++ src/main/java/com/access/erp/repo/RFQuotationRepo.java
@@ -3,8 +3,10 @@ package com.access.erp.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.access.erp.model.GateEntry;
 import com.access.erp.model.RfQuotation;
 
 @Repository
@@ -12,4 +14,13 @@ public interface RFQuotationRepo extends JpaRepository<RfQuotation, String>{
 
 	public RfQuotation findByRfqNo(String rfqNo);
 	public List<RfQuotation> findAllByOrderByRfqNoDesc();
+	
+	/*
+	 * @Query(
+	 * value="select g from GateEntry g where g.gateSrNo not in (select m.gateEntryNo from MRN m )"
+	 * ) public List<GateEntry> findAllNotInMrn();
+	 */
+	
+	@Query(value="select r from RfQuotation r where r.rfqNo not in (select q.rfqNo from QuotationDetail q)")
+	public List<RfQuotation> findAllRfqNotINQuotaion();
 }
