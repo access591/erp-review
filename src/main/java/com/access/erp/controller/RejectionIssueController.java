15fdc27847e42b50743e74122bfbeb4c0b3f4545
simr <simran.kaur16699@gmail.com>
2022-01-24 14:02:50 +0530

    code commit @Chanchal
    RejectionIssueform completed

--- src/main/java/com/access/erp/controller/RejectionIssueController.java
+++ src/main/java/com/access/erp/controller/RejectionIssueController.java
@@ -1,12 +1,16 @@
 package com.access.erp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.access.erp.service.RejectionIssueService;
 import  com.access.erp.model.RejectionIssue;
@@ -30,8 +34,8 @@ public class RejectionIssueController {
 	@PostMapping("/")
 	public String addRejectionIssue(@ModelAttribute("RejectionIssue")RejectionIssue rejectionIssue) {
 		
-		System.out.println();
-		
+		System.out.println("testing :" + rejectionIssue.getReqDate()  );
+		System.out.println("rate :" + rejectionIssue.getListRejectionIssueDetail().get(0).getIssrate());
 		rejectionIssueService.addRejectionIssue(rejectionIssue);
 		
 		return"redirect:/rejectionissue/";
@@ -42,11 +46,36 @@ public class RejectionIssueController {
 	@GetMapping("/list")
 	public String RejectionIssueList(Model model) {
 		
-		
-		
-		return"";
+		List<RejectionIssue> ListRejectionIssue = rejectionIssueService.getAllRejectionIssue();
+		if(ListRejectionIssue != null) {
+			model.addAttribute("RejectionIssueList",ListRejectionIssue);
+		}
+				
+				
+		return"layouts/listview/listofRejectionIssue";
 	}
 	
 	
+	@GetMapping("/edit/{id}")
+	public String editRejectionIssue(@PathVariable("id") String IssueNo,Model model) {
 	
+		RejectionIssue rejectionIssue= rejectionIssueService.editRejectionIssue(IssueNo).get();
+		model.addAttribute("RejectionIsssue",rejectionIssue);
+	
+		
+	return"layouts/editview/editRejectionIssue";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteRejectionIssue(@PathVariable("id") String IssueNo ,Model model,RedirectAttributes redirectAttributes) {
+		rejectionIssueService.deleteRejectionIssue(IssueNo);
+		
+		 redirectAttributes.addFlashAttribute("message","RejectionIssue  has been deleted successfully!");
+		  redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		  
+		
+		  return "redirect:/rejectionissue/list"; 
+	}
 }
+	
+	
--- src/main/java/com/access/erp/service/impl/RejectionIssueServiceImpl.java
+++ src/main/java/com/access/erp/service/impl/RejectionIssueServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.access.erp.model.PurchaseReturnDetail;
 import com.access.erp.model.RejectionIssue;
+import com.access.erp.model.RejectionIssueDetail;
 import com.access.erp.repo.RejectionIssueRepo;
 import com.access.erp.repo.SeqMainRepo;
 import com.access.erp.service.RejectionIssueService;
@@ -23,15 +24,17 @@ public class RejectionIssueServiceImpl implements RejectionIssueService {
 		if (rejectionissue.getIssNo() == "" | (rejectionissue.getIssNo() == null)) {
 			System.out.println("purchase return  code is : " + rejectionissue.getIssNo());
 
-			String maxCode = seqMainRepo.findByTranTypeAndFyCodeAndCCode("RISS", "20-21", "EB");
+			String maxCode = seqMainRepo.findByTranTypeAndFyCodeAndCCode("RIS", "20-21", "EB");
 
 			System.out.println("max code is : " + maxCode);
 			rejectionissue.setIssNo(maxCode);
+			System.out.println("IssueNo----->>>>>>> "+ rejectionissue.getIssNo());
 		}
-
-		
-		
-
+         
+		for(RejectionIssueDetail detail : rejectionissue.getListRejectionIssueDetail()) {
+		detail.setRejectionIssue(rejectionissue);
+			
+		}
 		rejectionIssuerepo.save(rejectionissue);
 
 	
